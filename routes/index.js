84d5e8a8addc3ce84aea12d339bea1fcc0d9d6d3
programzheng <programzheng@gmail.com>
2022-02-15 23:55:48 +0800

    remove: test_send_mail

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,9 @@ const express = require('express');
 const router = express.Router();
 const pug = require('pug');
 
-/**
- * Services
- */
-const { Mailer } = require(__dirname + '/../services/mailer.js');
-const mailer = new Mailer
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/test_send_mail', async (req, res, next) => {
-  await mailer.sendMailByHtml('foo@example.com', 'bar@example.com', 'hello world!', pug.renderFile('./views/index.pug', {
-    title: 'Timothy'
-  }))
 });
 
 module.exports = router;
